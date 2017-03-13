b90a9b0b7fa3bb2d94c27093e9ca19d93617d49c
Srikanth Uyyala <suyyala@codeaurora.org>
2017-03-13 12:17:39 +0530

    msm: camera: isp: use NOMINAL clock for CX iPeak
    Recommendation is to use NOMINAL in stead of TURBO
    clock as threshold for voting with CX iPeak.
    
    Change-Id: Idcdc9ee86118a34a031b80dc86f7cc6dad71a05f
    Signed-off-by: Srikanth Uyyala <suyyala@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp47.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp47.c
@@ -2610,11 +2610,11 @@ int msm_vfe47_set_clk_rate(struct vfe_device *vfe_dev, long *rate)
 	 * reduced before we go to turbo
 	 */
 	if ((vfe_dev->vfe_cx_ipeak) &&
-		(vfe_dev->vfe_clk_info[clk_idx].clk_rate >=
-		vfe_dev->vfe_clk_rates[MSM_VFE_CLK_RATE_TURBO]
+		(vfe_dev->msm_isp_vfe_clk_rate >=
+		vfe_dev->vfe_clk_rates[MSM_VFE_CLK_RATE_NOMINAL]
 		[vfe_dev->hw_info->vfe_clk_idx]) &&
 		prev_clk_rate <
-		vfe_dev->vfe_clk_rates[MSM_VFE_CLK_RATE_TURBO]
+		vfe_dev->vfe_clk_rates[MSM_VFE_CLK_RATE_NOMINAL]
 		[vfe_dev->hw_info->vfe_clk_idx]) {
 		ret = cx_ipeak_update(vfe_dev->vfe_cx_ipeak, true);
 		if (ret) {
@@ -2633,11 +2633,11 @@ int msm_vfe47_set_clk_rate(struct vfe_device *vfe_dev, long *rate)
 	 * if voting done earlier
 	 */
 	if ((vfe_dev->vfe_cx_ipeak) &&
-		(vfe_dev->vfe_clk_info[clk_idx].clk_rate <
-		vfe_dev->vfe_clk_rates[MSM_VFE_CLK_RATE_TURBO]
+		(vfe_dev->msm_isp_vfe_clk_rate <
+		vfe_dev->vfe_clk_rates[MSM_VFE_CLK_RATE_NOMINAL]
 		[vfe_dev->hw_info->vfe_clk_idx]) &&
 		prev_clk_rate >=
-		vfe_dev->vfe_clk_rates[MSM_VFE_CLK_RATE_TURBO]
+		vfe_dev->vfe_clk_rates[MSM_VFE_CLK_RATE_NOMINAL]
 		[vfe_dev->hw_info->vfe_clk_idx]) {
 		ret = cx_ipeak_update(vfe_dev->vfe_cx_ipeak, false);
 		if (ret) {
