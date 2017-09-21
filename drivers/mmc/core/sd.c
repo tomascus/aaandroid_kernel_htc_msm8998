htc-msm8998/kernel_htc_msm8998

mmc: import HTC changes
Change-Id: I118f1ca83915a92b15785f04bdbb6c3f6d589bf9

drivers/mmc/core/sd.c
@@ -230,7 +230,8 @@ static int mmc_decode_scr(struct mmc_card *card)
  */
 static int mmc_read_ssr(struct mmc_card *card)
 {
-	unsigned int au, es, et, eo;
+	unsigned int au, es, et, eo, spd_cls;
+	unsigned char card_spd_val[5] = {0, 2, 4, 6, 10};
 	int err, i;
 	u32 *ssr;
 
@@ -270,10 +271,23 @@ static int mmc_read_ssr(struct mmc_card *card)
 				card->ssr.erase_timeout = (et * 1000) / es;
 				card->ssr.erase_offset = eo * 1000;
 			}
+			printk(KERN_INFO "%s: au : %d KB\n",
+				mmc_hostname(card->host), card->ssr.au);
 		} else {
 			pr_warn("%s: SD Status: Invalid Allocation Unit size\n",
 				mmc_hostname(card->host));
 		}
+	}
+	spd_cls = UNSTUFF_BITS(ssr, 440 - 384, 8);
+	if (spd_cls < 5 && spd_cls > 0) {
+		printk(KERN_INFO "%s: speed class type is CLASS %d\n",
+			mmc_hostname(card->host), card_spd_val[spd_cls]);
+		card->speed_class = card_spd_val[spd_cls];
+	}
+	else {
+		printk(KERN_INFO "%s: Unknown speed class type\n",
+			mmc_hostname(card->host));
+		card->speed_class = -1;
 	}
 out:
 	kfree(ssr);
@@ -571,11 +585,11 @@ static int sd_set_current_limit(struct mmc_card *card, u8 *status)
 		err = mmc_sd_switch(card, 1, 3, current_limit, status);
 		if (err)
 			return err;
-
+#if 0
 		if (((status[15] >> 4) & 0x0F) != current_limit)
 			pr_warn("%s: Problem setting current limit!\n",
 				mmc_hostname(card->host));
-
+#endif
 	}
 
 	return 0;
@@ -1122,16 +1136,21 @@ static void mmc_sd_remove(struct mmc_host *host)
 	mmc_remove_card(host->card);
 
 	mmc_claim_host(host);
+	host->removed_cnt++;
 	host->card = NULL;
 	mmc_release_host(host);
 }
 
 /*
  * Card detection - card is alive.
+ * We don`t card card is alive or not
+ * If SD can be detect, we`ll reinit it anyway.
+ * return 0 : SD alive, inserted
+ * return 1 : SD dead, ejected
  */
 static int mmc_sd_alive(struct mmc_host *host)
 {
-	return mmc_send_status(host->card, NULL);
+	return !host->ops->get_cd(host);
 }
 
 /*
@@ -1140,9 +1159,6 @@ static int mmc_sd_alive(struct mmc_host *host)
 static void mmc_sd_detect(struct mmc_host *host)
 {
 	int err = 0;
-#ifdef CONFIG_MMC_PARANOID_SD_INIT
-	int retries = 5;
-#endif
 
 	BUG_ON(!host);
 	BUG_ON(!host->card);
@@ -1162,24 +1178,7 @@ static void mmc_sd_detect(struct mmc_host *host)
 	/*
 	 * Just check if our card has been removed.
 	 */
-#ifdef CONFIG_MMC_PARANOID_SD_INIT
-	while(retries) {
-		err = mmc_send_status(host->card, NULL);
-		if (err) {
-			retries--;
-			udelay(5);
-			continue;
-		}
-		break;
-	}
-	if (!retries) {
-		printk(KERN_ERR "%s(%s): Unable to re-detect card (%d)\n",
-		       __func__, mmc_hostname(host), err);
-		err = _mmc_detect_card_removed(host);
-	}
-#else
 	err = _mmc_detect_card_removed(host);
-#endif
 
 	mmc_put_card(host->card);
 
@@ -1190,6 +1189,8 @@ static void mmc_sd_detect(struct mmc_host *host)
 		mmc_detach_bus(host);
 		mmc_power_off(host);
 		mmc_release_host(host);
+		printk(KERN_ERR "%s %s removed %d times\n",
+			__func__, mmc_hostname(host), host->removed_cnt);
 	}
 }
 
@@ -1257,6 +1258,10 @@ static int _mmc_sd_resume(struct mmc_host *host)
 	BUG_ON(!host);
 	BUG_ON(!host->card);
 
+	host->crc_count = 0;
+	/* Recover Host capabilities */
+	host->caps |= host->caps_uhs;
+
 	mmc_claim_host(host);
 
 	if (!mmc_card_suspended(host->card))
@@ -1266,6 +1271,12 @@ static int _mmc_sd_resume(struct mmc_host *host)
 #ifdef CONFIG_MMC_PARANOID_SD_INIT
 	retries = 5;
 	while (retries) {
+		if (host->ops->get_cd && host->ops->get_cd(host) == 0) {
+			printk(KERN_ERR "%s(%s): find no card (%d). Stop trying\n",
+				 __func__, mmc_hostname(host), err);
+			break;
+		}
+		mmc_power_up(host, host->card->ocr);
 		err = mmc_sd_init_card(host, host->card->ocr, host->card);
 
 		if (err) {
@@ -1436,6 +1447,7 @@ int mmc_attach_sd(struct mmc_host *host)
 	if (!retries) {
 		printk(KERN_ERR "%s: mmc_sd_init_card() failure (err = %d)\n",
 		       mmc_hostname(host), err);
+		host->removed_cnt++;
 		goto err;
 	}
 #else

tomascus/aaandroid_kernel_htc_msm8998

drivers/mmc/core/sd.c
@@ -230,7 +230,8 @@ static int mmc_decode_scr(struct mmc_card *card)
  */
 static int mmc_read_ssr(struct mmc_card *card)
 {
-	unsigned int au, es, et, eo;
+	unsigned int au, es, et, eo, spd_cls;
+	unsigned char card_spd_val[5] = {0, 2, 4, 6, 10};
 	int err, i;
 	u32 *ssr;
 
@@ -270,10 +271,23 @@ static int mmc_read_ssr(struct mmc_card *card)
 				card->ssr.erase_timeout = (et * 1000) / es;
 				card->ssr.erase_offset = eo * 1000;
 			}
+			printk(KERN_INFO "%s: au : %d KB\n",
+				mmc_hostname(card->host), card->ssr.au);
 		} else {
 			pr_warn("%s: SD Status: Invalid Allocation Unit size\n",
 				mmc_hostname(card->host));
 		}
+	}
+	spd_cls = UNSTUFF_BITS(ssr, 440 - 384, 8);
+	if (spd_cls < 5 && spd_cls > 0) {
+		printk(KERN_INFO "%s: speed class type is CLASS %d\n",
+			mmc_hostname(card->host), card_spd_val[spd_cls]);
+		card->speed_class = card_spd_val[spd_cls];
+	}
+	else {
+		printk(KERN_INFO "%s: Unknown speed class type\n",
+			mmc_hostname(card->host));
+		card->speed_class = -1;
 	}
 out:
 	kfree(ssr);
@@ -571,11 +585,11 @@ static int sd_set_current_limit(struct mmc_card *card, u8 *status)
 		err = mmc_sd_switch(card, 1, 3, current_limit, status);
 		if (err)
 			return err;
-
+#if 0
 		if (((status[15] >> 4) & 0x0F) != current_limit)
 			pr_warn("%s: Problem setting current limit!\n",
 				mmc_hostname(card->host));
-
+#endif
 	}
 
 	return 0;
@@ -1099,6 +1113,7 @@ static int mmc_sd_init_card(struct mmc_host *host, u32 ocr,
 	card->clk_scaling_highest = mmc_sd_get_max_clock(card);
 	card->clk_scaling_lowest = host->f_min;
 
+	host->underclocking = 0;
 	return 0;
 
 free_card:
@@ -1122,16 +1137,21 @@ static void mmc_sd_remove(struct mmc_host *host)
 	mmc_remove_card(host->card);
 
 	mmc_claim_host(host);
+	host->removed_cnt++;
 	host->card = NULL;
 	mmc_release_host(host);
 }
 
 /*
  * Card detection - card is alive.
+ * We don`t card card is alive or not
+ * If SD can be detect, we`ll reinit it anyway.
+ * return 0 : SD alive, inserted
+ * return 1 : SD dead, ejected
  */
 static int mmc_sd_alive(struct mmc_host *host)
 {
-	return mmc_send_status(host->card, NULL);
+	return !host->ops->get_cd(host);
 }
 
 /*
@@ -1140,9 +1160,6 @@ static int mmc_sd_alive(struct mmc_host *host)
 static void mmc_sd_detect(struct mmc_host *host)
 {
 	int err = 0;
-#ifdef CONFIG_MMC_PARANOID_SD_INIT
-	int retries = 5;
-#endif
 
 	BUG_ON(!host);
 	BUG_ON(!host->card);
@@ -1162,24 +1179,7 @@ static void mmc_sd_detect(struct mmc_host *host)
 	/*
 	 * Just check if our card has been removed.
 	 */
-#ifdef CONFIG_MMC_PARANOID_SD_INIT
-	while(retries) {
-		err = mmc_send_status(host->card, NULL);
-		if (err) {
-			retries--;
-			udelay(5);
-			continue;
-		}
-		break;
-	}
-	if (!retries) {
-		printk(KERN_ERR "%s(%s): Unable to re-detect card (%d)\n",
-		       __func__, mmc_hostname(host), err);
-		err = _mmc_detect_card_removed(host);
-	}
-#else
 	err = _mmc_detect_card_removed(host);
-#endif
 
 	mmc_put_card(host->card);
 
@@ -1190,6 +1190,8 @@ static void mmc_sd_detect(struct mmc_host *host)
 		mmc_detach_bus(host);
 		mmc_power_off(host);
 		mmc_release_host(host);
+		printk(KERN_ERR "%s %s removed %d times\n",
+			__func__, mmc_hostname(host), host->removed_cnt);
 	}
 }
 
@@ -1256,7 +1258,11 @@ static int _mmc_sd_resume(struct mmc_host *host)
 
 	BUG_ON(!host);
 	BUG_ON(!host->card);
-
+#if 0
+	/* Recover Host capabilities */
+	if (!host->underclocking)
+		host->caps |= host->caps_uhs;
+#endif
 	mmc_claim_host(host);
 
 	if (!mmc_card_suspended(host->card))
@@ -1266,6 +1272,12 @@ static int _mmc_sd_resume(struct mmc_host *host)
 #ifdef CONFIG_MMC_PARANOID_SD_INIT
 	retries = 5;
 	while (retries) {
+		if (host->ops->get_cd && host->ops->get_cd(host) == 0) {
+			printk(KERN_ERR "%s(%s): find no card (%d). Stop trying\n",
+				 __func__, mmc_hostname(host), err);
+			break;
+		}
+		mmc_power_up(host, host->card->ocr);
 		err = mmc_sd_init_card(host, host->card->ocr, host->card);
 
 		if (err) {
@@ -1436,6 +1448,7 @@ int mmc_attach_sd(struct mmc_host *host)
 	if (!retries) {
 		printk(KERN_ERR "%s: mmc_sd_init_card() failure (err = %d)\n",
 		       mmc_hostname(host), err);
+		host->removed_cnt++;
 		goto err;
 	}
 #else